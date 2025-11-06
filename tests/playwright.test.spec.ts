0679ed9ea55869d7d5e4e403170748db94dbf9e9
revasingh <revasingh9@yahoo.in>
2025-11-06 16:44:32 -0600

    Fix URL assertion in Handling Assertions test

--- tests/playwright.test.spec.ts
+++ tests/playwright.test.spec.ts
@@ -103,7 +103,7 @@ test('Handling Checkboxes',async({page})=>{
 
 test('Handling Assertions',async({page})=>{
      await page.goto("http://www.tizag.com/htmlT/htmlcheckboxes.php");
-     await expect(page).toHaveURL("/.*htmlcheckboxes.php/");
+     await expect(page).toHaveURL("http://www.tizag.com/htmlT/htmlcheckboxes.php");
      console.log('URL matched successfully');
      await expect(page).not.toHaveURL(/.*error/);
      console.log('No error on the page hence passed');
